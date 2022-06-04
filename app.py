f3fda2cb9070e38ada261ae3ef9c2db465ba2433
Aprochlometh <Aprochlometh@yahoo.com>
2022-06-04 13:45:05 -0600

    added database

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ from models import db, connect_db, Pet
 from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "postgresql:///adopt")
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
-print(app.config["SECRET_KEY"])
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 connect_db(app)
